a28dbbd0667a0ee153bdce751a113e7f16ed45ba
Lara <lara@laranewsom.com>
2023-06-15 13:02:00 -0500

    updating to leverage toSignals

--- reactive-patterns/src/app/services/product-signals.service.ts
+++ reactive-patterns/src/app/services/product-signals.service.ts
@@ -1,26 +1,29 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { delay, of, tap } from 'rxjs';
+import { delay, map, of, tap } from 'rxjs';
 import { Product } from '../models/product';
 import { PRODUCTS } from '../models/product-data.mock';
 import { Category } from '../models/category';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductSignalsService {
+  // Accessing the activated route directly in the service that consumes the product id query param
+  // Query params are globally available therefore should be consumed directly where they are used
   private readonly activatedRoute = inject(ActivatedRoute);
+  readonly selectedProductId = toSignal(this.activatedRoute.queryParams.pipe(
+    map((queryParams) => queryParams['productId']))
+  );
 
 
   readonly selectedCategory = signal<string>(Category.ALL);
-  readonly selectedProductId = signal<string | undefined>(undefined);
 
   // Http requests are async and return an observable
   // this code is simulating an http request
   private readonly products$ = of(PRODUCTS).pipe(delay(300));
-
-  readonly products = signal<Product[]>([]);
+  readonly products = toSignal(this.products$, {initialValue: []});
 
   readonly filteredProducts = computed(() => {
     if(this.selectedCategory() === Category.ALL) {
@@ -31,27 +34,12 @@ export class ProductSignalsService {
   });
 
   readonly selectedProduct = computed(() => {
-    return this.products().find((product) => product.id === this.selectedProductId())
+    // comparing lowercase values so the query params do not have to be case sensitive
+    return this.products().find((product) => product.id.toLowerCase() === this.selectedProductId().toLowerCase())
   });
 
   readonly homeProducts = computed(() => {
     const middle = Math.floor(this.products().length / 2);
     return [this.products()[0], this.products()[middle], this.products()[this.products().length -1]];
   });
-
-  constructor(){
-    // httpClient returns observables
-    this.products$
-      .pipe(
-        tap((products) => this.products.set(products)),
-        takeUntilDestroyed()
-      )
-      .subscribe();
-
-    // Activated Route returns observables
-    this.activatedRoute.queryParams.pipe(
-        tap((queryParams) => this.selectedProductId.set(queryParams['productId'])),
-        takeUntilDestroyed(),
-      ).subscribe();
-  }
 }
